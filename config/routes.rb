0f40285a233d7f1bfe30529cbed0f1c255f03701
James Kay-Shuttleworth <kayshuttleworthjames@gmail.com>
2025-05-20 16:12:50 +0100

    Completed the enitirty of the To do list app

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,10 @@ Rails.application.routes.draw do
   get "/tasks/new", to: "tasks#new", as: :new
   post "/tasks", to: "tasks#create"
 
-  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
   patch "/tasks/:id", to: "tasks#update"
 
   get "/tasks/:id", to: "tasks#show", as: :task
+
+  delete "tasks/:id", to: "tasks#destroy", as: :delete
 end
